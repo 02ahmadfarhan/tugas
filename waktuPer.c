b6d43846f7cdd3191eed8f0da2aadc967e37b736
02ahmadfarhan <02ahmadfarhan@gmail.com>
2023-10-19 21:11:28 +0700

    update cpp and c

--- waktuPer.c
+++ waktuPer.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    /*Algoritma Lama Percakapan 4000 detik {
+        maka 4000 detik = 1 jam + 6 menit + 40 detik
+    }*/
 
-    // deklarasi variable
-    int jam = 4000 / 3600;
+    // deklarasi variable dan nilai
+    int jam = 4000 / 3600; // ()
     int sisaDetik = 4000 % 3600;
     int menit = sisaDetik / 60;
     int detik = sisaDetik % 60;
 
-    // lama percakapan
-    int total =  jam + menit + detik;
+    // jumlah lama percakapan
+    int jumlah =  jam + menit + detik;
 
     // hasil
-    printf("Hasil lama percakapan = jam %d, menit %d, detik %d\n", jam, menit, detik);
-    printf("total = %d\n", total);
+    printf("Hasil Lama Percakapan = jam %d, menit %d, detik %d\n", jam, menit, detik);
+    printf("jumlah = %d\n", jumlah);
 }
