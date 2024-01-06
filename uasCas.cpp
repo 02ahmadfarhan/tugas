b15ad461402b3ad20126f066e8490650fffb0338
02ahmadfarhan <02ahmadfarhan@gmail.com>
2024-01-06 22:17:21 +0700

    code uas menggunakan case, tadinya sih if, tapi rencananya mau menambahkan for/ pengulangan di code ini, apakah bisa, i'll try

---
+++ uasCas.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    string namaPaket;
+    int nomor,harga,banyak,total,pajak,jumlah;
+
+    cout << "Pilih Menu Minuman di Udin Coffee " << endl;
+    cout << "1. Paket VIP\n";
+    cout << "2. Paket Spesial\n";
+    cout << "3. Paket Super" << endl;
+
+    cout << "Pilih paket yang kamu mau : "; cin >> nomor;
+    
+}
